c4617d18ff248d53daff7dea463f752dc795d778
xiangjun02 <>
2022-04-01 23:07:35 +0800

    feat: test

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,3 +1,10 @@
+/*
+ * @Descripttion: 
+ * @Author: xiangjun02
+ * @Date: 2022-04-01 22:48:45
+ * @LastEditors: xiangjun02
+ * @LastEditTime: 2022-04-01 22:59:30
+ */
 export const Popup = () => {
-  return <div>Hey!</div>;
+  return <div>Hey i am daxiang!</div>;
 };

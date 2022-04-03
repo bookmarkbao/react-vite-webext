120ec367eb9f5b3d6b9be59507fb5e86cab5586c
xiangjun02 <>
2022-04-04 00:58:48 +0800

    feat: 集成数据列表

--- src/popup/main.tsx
+++ src/popup/main.tsx
@@ -1,3 +1,10 @@
+/*
+ * @Descripttion: 
+ * @Author: xiangjun02
+ * @Date: 2022-04-04 00:51:12
+ * @LastEditors: xiangjun02
+ * @LastEditTime: 2022-04-04 00:55:32
+ */
 import "../styles";
 import React from "react";
 import ReactDOM from "react-dom";
--- src/views/main.tsx
+++ src/views/main.tsx
@@ -1,11 +1,18 @@
+/*
+ * @Descripttion: 
+ * @Author: xiangjun02
+ * @Date: 2022-04-04 00:51:22
+ * @LastEditors: xiangjun02
+ * @LastEditTime: 2022-04-04 00:54:53
+ */
 import "../styles";
 import React from "react";
 import ReactDOM from "react-dom";
-import { Popup } from "./Popup";
+import MainConent from "~/components/MainApp";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Popup />
+    <MainConent />
   </React.StrictMode>,
   document.getElementById("root")
 );

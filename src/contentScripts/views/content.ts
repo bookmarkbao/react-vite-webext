e240f92a4a474ed7c32351536d4797bb8a6b4d61
xiangjun02 <>
2022-04-04 18:31:25 +0800

    feat: content宽度可配置

--- src/contentScripts/views/content.ts
+++ src/contentScripts/views/content.ts
@@ -3,12 +3,13 @@
  * @Author: xiangjun02
  * @Date: 2022-04-04 00:41:45
  * @LastEditors: xiangjun02
- * @LastEditTime: 2022-04-04 18:05:16
+ * @LastEditTime: 2022-04-04 18:28:42
  */
 
 
 let iframe;
-
+// const iframeW = 850
+const iframeW = 320
 // 只在最顶层页面嵌入iframe
 if (window.self === window.top) {
   document.onreadystatechange = () => {
@@ -16,7 +17,7 @@ if (window.self === window.top) {
       iframe = document.createElement('iframe'); 
       iframe.className = "api-interceptor";
       iframe.style.setProperty('height', '100%', 'important');
-      iframe.style.setProperty('width', '850px', 'important');
+      iframe.style.setProperty('width', `${iframeW}px`, 'important');
       iframe.style.setProperty('min-width', '1px', 'important');
       iframe.style.setProperty('position', 'fixed', 'important');
       iframe.style.setProperty('top', '0', 'important');
@@ -24,14 +25,14 @@ if (window.self === window.top) {
       iframe.style.setProperty('left', 'auto', 'important');
       iframe.style.setProperty('bottom', 'auto', 'important');
       iframe.style.setProperty('z-index', '9999999999999', 'important');
-      iframe.style.setProperty('transform', 'translateX(870px)', 'important');
+      iframe.style.setProperty('transform', `translateX(${iframeW+20}px)`, 'important');
       iframe.style.setProperty('transition', 'all .4s', 'important');
       iframe.style.setProperty('box-shadow', '0 0 15px 2px rgba(0,0,0,0.12)', 'important');
       iframe.frameBorder = "none"; 
       iframe.src = chrome.extension.getURL("./dist/views/index.html")
       document.body.appendChild(iframe);
       let show = true;
-      iframe.style.setProperty('transform', show ? 'translateX(0)' : 'translateX(870px)', 'important');
+      iframe.style.setProperty('transform', show ? 'translateX(0)' : `translateX(${iframeW+20}px)`, 'important');
     }
   }
 }
